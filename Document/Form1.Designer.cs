d9bb6911ef47f971b1f15a5b42955fcffe15991c
danilo dos santos <danilo.dossantos@jala.university>
2024-02-22 00:26:54 -0300

    Add Stream Writer

---
+++ Document/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Document
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.Load = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.listView1.HideSelection = false;
+            this.listView1.LabelEdit = true;
+            this.listView1.Location = new System.Drawing.Point(127, 69);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(629, 301);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(772, 306);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 29);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add Item";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(772, 341);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 29);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Remove Item";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(127, 391);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(86, 34);
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(229, 391);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(86, 34);
+            this.btnImport.TabIndex = 4;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(671, 391);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(85, 34);
+            this.Load.TabIndex = 4;
+            this.Load.Text = "To save";
+            this.Load.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(912, 556);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Document";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button Load;
+    }
+}
+
---
+++ Document/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Document
+{
+    public partial class Form1 : Form
+    {
+
+   
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeView();
+           
+        }
+
+        private void InitializeView()
+        {
+            listView1.View = View.Details;
+            listView1.Columns.Add("Text message:", -2, HorizontalAlignment.Right);
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Abre uma janela de diálogo para salvar o arquivo
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "Text file|*.txt";
+                saveFileDialog1.Title = "Save text file";
+                saveFileDialog1.ShowDialog();
+
+                // Se o user pressionar OK
+                if (saveFileDialog1.FileName != "")
+                {
+                    // Cria um StreamWriter para escrever no arquivo
+                    using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                    {
+                        //Escreve cada item do ListView no arquivo
+                        foreach (ListViewItem item in listView1.Items)
+                        {
+                            sw.WriteLine(item.Text);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+              
+                RegisterExcecao(ex);
+                MessageBox.Show("Ocorreu um erro ao exportar as informações.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //Abre uma janela de dialogo para selecionar o arquivo
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Filter = "Text file|*.txt";
+                openFileDialog1.Title = "Select text file";
+                openFileDialog1.ShowDialog();
+
+                //Se o user pressionar OK
+                if (openFileDialog1.FileName != "")
+                {
+
+                    listView1.Items.Clear();
+                    //Cria um StreamReader para ler o arquivo
+                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                    {
+                        string line;
+                        // Lê cada linha do arquivo e adiciona ListView
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            string[] parts = line.Split(',');
+                            listView1.Items.Add(new ListViewItem(parts));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+               
+                RegisterExcecao(ex);
+                MessageBox.Show("Ocorreu um erro ao importar as informações.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Add("Edit Here (click 3x):");
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+
+            if (listView1.Items.Count > 0)
+                listView1.Items.RemoveAt(listView1.Items.Count - 1);
+        }
+
+        private void RegisterExcecao(Exception ex)
+        {
+            string logFilePath = "Error.log.txt";
+            using (StreamWriter sw = File.AppendText(logFilePath)) 
+            {
+                sw.WriteLine($"[{DateTime.Now} {ex.GetType()}: {ex.Message}]");
+                sw.WriteLine($"StackTrace: {ex.StackTrace}");
+                sw.WriteLine();
+            }
+        }
+    }
+}
